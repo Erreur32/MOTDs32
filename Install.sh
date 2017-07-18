29cde1be84bb889cddaac78d662822f60a8345a4
Erreur32 <erreur32@echosystem.fr>
2017-07-18 23:39:13 +0200

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -12,7 +12,7 @@
 #
 
 
-echo -n "Do you wish to install the missing package (y/n)? "
+echo -n "Do you wish to install the missing package before (y/n)?  install  ntp figlet "
 old_stty_cfg=$(stty -g)
 stty raw -echo
 answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
