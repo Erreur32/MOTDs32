f58b5be196eb9ef0953aae76b28588f5ef593512
Erreur32 <erreur32@echosystem.fr>
2017-07-20 15:55:10 +0200

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -66,12 +66,12 @@ answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
 stty $old_stty_cfg
 if echo "$answer" | grep -iq "^y" ;then
     echo -e "\n 1.\e[92m installation \e[0m  ntp figlet \e[92m start\e[0m \n" ;  apt-get install -y  ntp figlet; make install 
-    echo -e "\n\e[92m apt-get installation \e[0m Done \n"
+    echo -e "\n\e[92m apt-get installation \e[0m Succesfull  \n"
 else
     echo -e "\n 1.\e[92m ok no apt-get required \e[0m Installation continue... \n";  make install 
 fi
 
-echo -e "\n"
+echo -e "Make command inprogress"
 
 ## Install Crontab
 
@@ -88,6 +88,6 @@ echo -e " 2.\e[92m ROOT crontab\e[0m ADD (generation each 5 minutes)\n"
 ## Generate first stats
 /usr/bin/motds32 -g
 
-echo -e "\n 3.\e[92mInstallation successfull !!\e[0m  \n  Use: /usr/bin/motds32 for any help\n"
+echo -e "\n 3.\e[92mMOTDs32 Installation completed!\e[0m  \n  Use: /usr/bin/motds32 for any help\n"
 
 #EOF 
