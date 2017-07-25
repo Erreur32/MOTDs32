ef88f7766c8e253ee19cbe57ea7f96e043b373a5
Erreur32 <postmaster@echosystem.fr>
2017-07-25 21:05:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -121,10 +121,9 @@ stty $old_stty_cfg
 if echo "$answer" | grep -iq "^y" ;then
     echo -e "\n\e[92m - apt-get install \e[0m  ntp figlet build-essential  \e[94m start\e[0m\n" ;  apt-get install -y build-essential ntp figlet;
     echo -e "\n\e[92m - apt-get installation\e[0m Succesfull\n"
-    echo -e "\n\e[92m - Building binary \e[0m\n"
-    if [ -f "/usr/bin/motds32" ]
+     if [ -f "/usr/bin/motds32" ]
       then
-      echo -e "\n\e[92m - Make file\e[0m done.\n"
+      echo -e "\n\e[92m - Make file already\e[0m done.\n"
      else 
       echo -e "\n\e[92m -Building binary \e[0m\n"
       make install
@@ -137,8 +136,8 @@ stty raw -echo
 answer2=$( while ! head -c 2 | grep -i '[ny]' ;do true ;done )
 stty $old2_stty_cfg
   if echo "$answer2" | grep -iq "^y" ;then
-  echo -e "\n\e[92m No apt-get required\e[0m Installation continue... \n";  
-  if [ -f "/usr/bin/motds32" ]
+  echo -e "\n\e[92m - NO apt-get required\e[0m Installation continue... \n";  
+    if [ -f "/usr/bin/motds32" ]
       then
       echo -e "\n\e[92m - Make file already\e[0m done.\n"
      else 
