82bdd17c66f8cb7d37b7e38dd1dcd4ac76690231
Erreur32 <postmaster@echosystem.fr>
2017-07-25 20:43:04 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -74,7 +74,7 @@ if [ `grep -c /etc/motd /root/.bashrc` == 0 ]
 then
     echo -e " code bash missing"
 echo "cat /etc/motd" >> /root/.bashrc
-echo -e "\e[92m Code bash \e[0madded.\n"
+echo -e "\e[92m - Code bash \e[0madded.\n"
 else
 grepmot=`grep  '/etc/motd' /root/.bashrc`
     echo -e "Code bash found $grepmot \n" 
@@ -86,13 +86,13 @@ fi
 if [ -f  "/usr/share/figlet/" ]
  then
    echo -e "  -->\e[34m  Copy font figlet\e[0m"
- cp fonts/chunky.flf /usr/share/figlet/ 
+ cp -f fonts/chunky.flf /usr/share/figlet/ 
 fi
 
 
 if [ -f  "/etc/motds32/Stats32" ]
  then
-   echo -e "  -->\e[34m  Stats32 already installed\e[0m"
+   echo -e "  -->\e[34m  - Stats32 already installed\e[0m"
  else
    mkdir /etc/motds32;
    cp  Stats32 /etc/motds32/Stats32 -Rf
@@ -118,16 +118,16 @@ stty raw -echo
 answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
 stty $old_stty_cfg
 if echo "$answer" | grep -iq "^y" ;then
-    echo -e "\n\e[92m apt-get install \e[0m  ntp figlet build-essential  \e[94m start\e[0m\n" ;  apt-get install -y build-essential ntp figlet;
-    echo -e "\n\e[92m apt-get installation\e[0m Succesfull\n"
-    echo -e "\n\e[92m  Building binary \e[0m\n"
+    echo -e "\n\e[92m - apt-get install \e[0m  ntp figlet build-essential  \e[94m start\e[0m\n" ;  apt-get install -y build-essential ntp figlet;
+    echo -e "\n\e[92m - apt-get installation\e[0m Succesfull\n"
+    echo -e "\n\e[92m - Building binary \e[0m\n"
     if [ -f /usr/bin/motds32 ]
       then
-      echo -e "\n\e[92m Make file\e[0m done.\n"
+      echo -e "\n\e[92m - Make file\e[0m done.\n"
      else 
-      echo -e "\n\e[92m  Building binary \e[0m\n"
+      echo -e "\n\e[92m -Building binary \e[0m\n"
       make install
-      echo -e "\n\e[92m Make file\e[0m done.\n"
+      echo -e "\n\e[92m - Make file\e[0m done.\n"
      fi
 else
 echo -e "\n\e[34mDo you want to continue this installation? (y/n)\n "
