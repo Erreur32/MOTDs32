b368eec4b9d44fb076422a635906092deb6795b2
Erreur32 <postmaster@echosystem.fr>
2017-07-31 15:25:00 +0200

    modif makefile

--- install.sh
+++ install.sh
@@ -165,7 +165,7 @@ tput cup 11 0
         (crontab -l ; echo "*/5 * * * *      /usr/bin/motds32 -g 2>1")| crontab -
         fi
 ## Generate first stats
-      /usr/bin/motds32 -g
+if test -f /usr/bin/motds32; /usr/bin/motds32 -g; fi
 
       echo -e "\n\n\e[34m \e[92m  Ｉｎｓｔａｌｌａｔｉｏｎ  ｏｆ  ＭＯＴＤｓ３２ completed!\e[0m  \n\n\n Help? Use: \n \033[1;30m /usr/bin/motds32\e[0m \n or \033[1;30m\n  motd\e[0m\n"
       printf "\n   \e[1;33m  (Press any key to exit.)\e[0m"
@@ -227,7 +227,8 @@ tput rmcup
           (crontab -l ; echo "*/5 * * * *      /usr/bin/motds32 -g 2>1")| crontab -
         fi
 ## Generate first stats
-      /usr/bin/motds32 -g
+ if test -f /usr/bin/motds32; /usr/bin/motds32 -g; fi
+
       echo -e "\n\n\e[34m \e[92m  Ｉｎｓｔａｌｌａｔｉｏｎ  ｏｆ  ＭＯＴＤｓ３２ completed!\e[0m  \n\n\n Help? Use: \n \033[1;30m /usr/bin/motds32\e[0m \n or \033[1;30m\n  motd\e[0m\n"
       printf "\n   \e[1;33m  (Press any key to exit.)\e[0m"
       read -n 1
@@ -271,7 +272,7 @@ tput rmcup
       esac
 
 ## Generate first stats
-      /usr/bin/motds32 -g
+if test -f /usr/bin/motds32; /usr/bin/motds32 -g; fi
 
       echo -e "\n\n\e[34m \e[92m  Ｉｎｓｔａｌｌａｔｉｏｎ  ｏｆ  ＭＯＴＤｓ３２ completed!\e[0m  \n\n\n Help? Use: \n \033[1;30m /usr/bin/motds32\e[0m \n or \033[1;30m\n  motd\e[0m\n"
       printf "\n   \e[1;33m  (Press any key to exit.)\e[0m"
