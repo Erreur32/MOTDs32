99175e27eea44b81d3a44277e7fbd0bff04edd22
Erreur32 <postmaster@echosystem.fr>
2017-07-31 19:08:20 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -164,7 +164,7 @@ tput cup 11 0
         (crontab -l ; echo "*/5 * * * *      /usr/bin/motds32 -g 2>1")| crontab -
         fi
 ## Generate first stats
-if test -f /usr/bin/motds32; then /usr/bin/motds32 -g; echo -e "\n  Genrate first Stat"; fi
+if test -f /usr/bin/motds32; then /usr/bin/motds32 -g; fi
 
       echo -e "\n\n\e[34m \e[92m  Ｉｎｓｔａｌｌａｔｉｏｎ  ｏｆ  ＭＯＴＤｓ３２ completed!\e[0m  \n\n\n Help? Use: \n \033[1;30m /usr/bin/motds32\e[0m \n or \033[1;30m\n  motd\e[0m\n"
       printf "\n   \e[1;33m  (Press any key to Continue.)\e[0m"
