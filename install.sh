ba1fabf07b33b3b3da445c4f3bf1e798b408256d
Erreur32 <postmaster@echosystem.fr>
2017-07-31 16:45:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -164,7 +164,7 @@ tput cup 11 0
         (crontab -l ; echo "*/5 * * * *      /usr/bin/motds32 -g 2>1")| crontab -
         fi
 ## Generate first stats
-if test -f /usr/bin/motds32; then /usr/bin/motds32 -g; echo "\n  Genrate first Stat"; fi
+if test -f /usr/bin/motds32; then /usr/bin/motds32 -g; echo -e "\n  Genrate first Stat"; fi
 
       echo -e "\n\n\e[34m \e[92m  Ｉｎｓｔａｌｌａｔｉｏｎ  ｏｆ  ＭＯＴＤｓ３２ completed!\e[0m  \n\n\n Help? Use: \n \033[1;30m /usr/bin/motds32\e[0m \n or \033[1;30m\n  motd\e[0m\n"
       printf "\n   \e[1;33m  (Press any key to Continue.)\e[0m"
@@ -219,7 +219,7 @@ if test -f /usr/bin/motds32; then /usr/bin/motds32 -g; echo "\n  Genrate first S
           (crontab -l ; echo "*/5 * * * *      /usr/bin/motds32 -g 2>1")| crontab -
         fi
 ## Generate first stats
-      if test -f /usr/bin/motds32; then /usr/bin/motds32 -g; echo "\n  Genrate first stat"; fi
+      if test -f /usr/bin/motds32; then /usr/bin/motds32 -g; echo -e "\n  Genrate first stat"; fi
 
       echo -e "\n\n\e[34m \e[92m  Ｉｎｓｔａｌｌａｔｉｏｎ  ｏｆ  ＭＯＴＤｓ３２ completed!\e[0m  \n\n\n Help? Use: \n \033[1;30m /usr/bin/motds32\e[0m \n or \033[1;30m\n  motd\e[0m\n"
       printf "\n   \e[1;33m  (Press any key to exit.)\e[0m"
@@ -260,7 +260,7 @@ if test -f /usr/bin/motds32; then /usr/bin/motds32 -g; echo "\n  Genrate first S
       esac
       read -n 1
 ## Generate first stats
-  if test -f /usr/bin/motds32; then /usr/bin/motds32 -g; echo "\n   genrate first stat"; fi
+  if test -f /usr/bin/motds32; then /usr/bin/motds32 -g; echo -e "\n   genrate first stat"; fi
 
 #      echo -e "\n\n\e[34m \e[92m  Ｉｎｓｔａｌｌａｔｉｏｎ  ｏｆ  ＭＯＴＤｓ３２ completed!\e[0m  \n\n\n Help? Use: \n \033[1;30m /usr/bin/motds32\e[0m \n or \033[1;30m\n  motd\e[0m\n"
 #      printf "\n   \e[1;33m  (Press any key to exit.)\e[0m"
