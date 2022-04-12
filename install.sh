98ab73f92baeb73d1b574758c418c92ce0184748
Erreur32 <Erreur32@users.noreply.github.com>
2022-04-12 16:16:16 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+{     "deaths": [         {             "animalsKilled": [],             "kills": [],             "longestShot": 0,             "timeSurvived": 382,             "zKillCount": 1,             "distTrav": 421,             "timeStamp": "2021-9-20 18:38:46",             "posDeath": "1045.71,236.857,290.303",             "killer": "suicide",             "weapon": "Tombstone"         },         {             "animalsKilled": [],             "kills": [],             "longestShot": 0,             "timeSurvived": 1517,             "zKillCount": 6,             "distTrav": 4485,             "timeStamp": "2021-9-20 21:34:20",             "posDeath": "14006.6,451.091,3831.18",             "killer": "suicide",             "weapon": "Tombstone"         },         {             "animalsKilled": [],             "kills": [                 {                     "victim": {                         "id": "76561197972131535",                         "timeSurvived": 12899                     },                     "timeStamp": "2021-9-23 22:37:16",                     "posKill": "6062.79,217.832,1673.89",                     "weapon": "TTC_HK416OD",                     "distance": 18.868499755859376                 }             ],             "longestShot": 666,             "timeSurvived": 9802,             "zKillCount": 109,             "distTrav": 29124,             "timeStamp": "2021-10-3 16:5:23",             "posDeath": "2053.1,268.742,11361.5",             "killer": "suicide",             "weapon": "Tombstone"         },         {             "animalsKilled": [],             "kills": [],             "longestShot": 3,             "timeSurvived": 12898,             "zKillCount": 53,             "distTrav": 75679,             "timeStamp": "2021-10-14 21:54:6",             "posDeath": "10542,331.114,7420.39",             "killer": "suicide",             "weapon": "Tombstone"         },         {             "animalsKilled": [                 "Animal_CervusElaphus",                 "Animal_SusScrofa",                 "Animal_SusScrofa",                 "Animal_SusScrofa",                 "Animal_SusScrofa",                 "Animal_SusScrofa",                 "Animal_SusDomesticus",                 "Animal_GallusGallusDomesticus"             ],             "kills": [],             "longestShot": 0,             "timeSurvived": 16965,             "zKillCount": 82,             "distTrav": 350461,             "timeStamp": "2021-11-3 14:43:43",             "posDeath": "6757.48,307.114,13411",             "killer": "natural cause",             "weapon": ""         },         {             "animalsKilled": [                 "Animal_OvisAries",                 "Animal_OvisAries",                 "Animal_OvisAries",                 "Animal_OvisAries",                 "Animal_CanisLupus_Grey",                 "Animal_CapraHircus_Black",                 "Animal_CapraHircusF",                 "Animal_CapraHircusF",                 "Animal_OvisAries",                 "Animal_OvisAries",                 "Animal_CapreolusCapreolus",                 "Animal_OvisAriesF",                 "Animal_OvisAriesF",                 "Animal_CervusElaphus",                 "Animal_UrsusArctos",                 "Animal_OvisAriesF",                 "Animal_UrsusArctos",                 "Animal_GallusGallusDomesticusF_White",                 "Animal_CanisLupus_Grey",                 "Animal_CervusElaphus",                 "Animal_CapraHircus_White",                 "Animal_BosTaurusF_Brown",                 "Animal_CapraHircus_Brown",                 "Animal_BosTaurusF_Spotted",                 "Animal_CapraHircus_Black",                 "Animal_CapraHircus_Brown"             ],             "kills": [                 {                     "victim": {                         "id": "76561197972131535",                         "timeSurvived": 45181                     },                     "timeStamp": "2022-3-31 22:31:26",                     "posKill": "11031.5,231.943,3607.71",                     "weapon": "TTC_M110_Black",                     "distance": 593.981201171875                 },                 {                     "victim": {                         "id": "76561197972131535",                         "timeSurvived": 1292                     },                     "timeStamp": "2022-3-31 22:53:42",                     "posKill": "11091.4,231.943,3606.38",                     "weapon": "TTC_MCX_Tan",                     "distance": 16.679922103881837                 }             ],             "longestShot": 1078,             "timeSurvived": 72296,             "zKillCount": 775,             "distTrav": 322485,             "timeStamp": "2022-4-2 17:44:40",             "posDeath": "8188.92,232.546,870.027",             "killer": "suicide",             "weapon": "CombatKnife"         }     ],     "kills": [],     "animalsKilled": [         "dbo_raptor",         "dbo_raptorB",         "dbo_raptorblue",         "dbo_raptorB",         "Animal_CanisLupus_Grey",         "Animal_CanisLupus_Grey",         "Animal_CanisLupus_White",         "Animal_CanisLupus_White",         "Animal_CanisLupus_Grey",         "dbo_trex",         "Animal_dbo_dil"     ],     "name": "TerrorEast",     "lastTimeSeen": "2022-4-3 21:12:38",     "deathsToZCount": 0,     "deathsToNaturalCauseCount": 1,     "deathsToPlayerCount": 0,     "deathsToAnimalCount": 0,     "suicideCount": 5,     "longestShot": 0,     "zKilled": 23,     "timeSurvived": 6720,     "distTrav": 56034 }#!/bin/sh
 #
 #  MOTDs32 installation instructions
 #  ==================================
@@ -38,7 +38,7 @@
 ################################################################################
 # Make sure only root can run our script
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 1>&2
+   /bin/echo "This script must be run as root" 1>&2
    exit 1
 fi
 
@@ -53,7 +53,7 @@ FG_WHITE="$(tput setaf 7)"
 tput smcup
 # Display menu until selection == 0
 while [[ $REPLY != 0 ]]; do
-echo -n ${BG_BLACK}${FG_WHITE}
+/bin/echo -n ${BG_BLACK}${FG_WHITE}
 clear
 cat <<- "EOFMO"
 
@@ -68,89 +68,89 @@ cat <<- "EOFMO"
     By  Ｅｒｒｅｕｒ３２
 
 EOFMO
-echo -e "\033[1;30m # MOTDs32 is dynamicaly refreshing the /etc/motd file with current informations"
-echo -e "\033[1;30m # about system status and usage."
-echo -e "\n \e[34m  Installation of ＭＯＴＤｓ３２ in progress... \e[0m\n\n"
+/bin/echo -e "\033[1;30m # MOTDs32 is dynamicaly refreshing the /etc/motd file with current informations"
+/bin/echo -e "\033[1;30m # about system status and usage."
+/bin/echo -e "\n \e[34m  Installation of ＭＯＴＤｓ３２ in progress... \e[0m\n\n"
 
 # Check dependenciess
-echo -e "\e[34m Check required dependencies ! (need to be installed)"
+/bin/echo -e "\e[34m Check required dependencies ! (need to be installed)"
 
 # Check installed
     if [ -f  "/usr/sbin/sendmail" ]
       then
-        echo -e "    \e[34m-  Posfix \e[92m OK\e[0m\n"
+        /bin/echo -e "    \e[34m-  Posfix \e[92m OK\e[0m\n"
       else
-        echo -e "    \e[34m-  Select option 1 \e[91m Missing package !! \e[0m\n"
+        /bin/echo -e "    \e[34m-  Select option 1 \e[91m Missing package !! \e[0m\n"
      fi
  
    if  [ -f  "/usr/share/build-essential/essential-packages-list" ]
     then
-  echo -e "    \e[34m-  build-dep       \e[32m✔ installed.\e[0m"
+  /bin/echo -e "    \e[34m-  build-dep       \e[32m✔ installed.\e[0m"
     else
-  echo -e "    \e[34m-  build-dep       \e[1;31mSelect option 1 \e[91m Missing package !!\e[0m"
+  /bin/echo -e "    \e[34m-  build-dep       \e[1;31mSelect option 1 \e[91m Missing package !!\e[0m"
     fi
 
 
 # Check files
 
 if hash figlet 2>/dev/null; then
-echo -e "    \e[34m-  Figlet          \e[32m✔ installed.\e[0m"
+/bin/echo -e "    \e[34m-  Figlet          \e[32m✔ installed.\e[0m"
     else
-echo -e "    \e[34m-  Figlet         \e[1;31m Select option 1 \e[91m Missing package !!\e[0m"
+/bin/echo -e "    \e[34m-  Figlet         \e[1;31m Select option 1 \e[91m Missing package !!\e[0m"
     fi
 if hash ntpd 2>/dev/null; then
-echo -e "    \e[34m-  NTP             \e[32m✔ installed.\e[0m"
+/bin/echo -e "    \e[34m-  NTP             \e[32m✔ installed.\e[0m"
     else
-echo -e "    \e[34m-  NTP           \e[1;31mSelect option 1 \e[91m Missing package !!\e[0m"
+/bin/echo -e "    \e[34m-  NTP           \e[1;31mSelect option 1 \e[91m Missing package !!\e[0m"
     fi
 if hash make 2>/dev/null; then
-echo -e "    \e[34m-  build-essential \e[32m✔ installed.\e[0m"
+/bin/echo -e "    \e[34m-  build-essential \e[32m✔ installed.\e[0m"
     else
-echo -e "    \e[34m-  build-essential  \e[1;31mSelect option 1 \e[91m Missing package !!\e[0m"
+/bin/echo -e "    \e[34m-  build-essential  \e[1;31mSelect option 1 \e[91m Missing package !!\e[0m"
     fi
 
 
-echo -e "\n        YOU CAN SKIP (auto) install if you want"
-echo -e "\n\e[92m  Please Select:"
-echo -e "        1. \e[0m(auto) Install Missing Package\e[92m"
-echo -e "        2. \e[0mInstallation MOTDs32 script\e[92m"
-echo -e "        3. \e[0mUninstall completly  MOTDs32 script\e[92m"
-echo -e "        0. \e[0m(Enter or any key) to Quit\n \e[0m"
+/bin/echo -e "\n        YOU CAN SKIP (auto) install if you want"
+/bin/echo -e "\n\e[92m  Please Select:"
+/bin/echo -e "        1. \e[0m(auto) Install Missing Package\e[92m"
+/bin/echo -e "        2. \e[0mInstallation MOTDs32 script\e[92m"
+/bin/echo -e "        3. \e[0mUninstall completly  MOTDs32 script\e[92m"
+/bin/echo -e "        0. \e[0m(Enter or any key) to Quit\n \e[0m"
 read -p "     Enter selection [0-3] or (Enter) to Quit > " selection
 
 
 # Clear area beneath menu
 tput cup 10 0
-echo -n "${BG_BLACK}${FG_GREEN}"
+/bin/echo -n "${BG_BLACK}${FG_GREEN}"
 tput ed
 tput cup 11 0
 
  # Act on selection
       case $selection in
-        1)  echo -e " \e[32m install missing package  ...\e[0m\n"
+        1)  /bin/echo -e " \e[32m install missing package  ...\e[0m\n"
             apt-get install  --allow-unauthenticated -y build-essential ntp figlet postfix sasl2-bin
-            echo -e "\n\e[34m - apt-get installation build-essential ntp figlet \e[0m>> Succesfull"
+            /bin/echo -e "\n\e[34m - apt-get installation build-essential ntp figlet \e[0m>> Succesfull"
      if [ -f "/usr/bin/motds32" ]
       then
-      echo -e "\n\e[34m - Make file \e[0m>> founded.\e[0m\n"
+      /bin/echo -e "\n\e[34m - Make file \e[0m>> founded.\e[0m\n"
       #make install
-      #echo -e "\n\e[34m - REMake file done.\e[0m\n"
+      #/bin/echo -e "\n\e[34m - REMake file done.\e[0m\n"
       else
-      echo -e "\n\e[92m -Building binary \e[0m\n"
+      /bin/echo -e "\n\e[92m -Building binary \e[0m\n"
       make install
-      echo -e "\n\e[34m - Make file done.\e[0m\n"
+      /bin/echo -e "\n\e[34m - Make file done.\e[0m\n"
      fi
 
-      echo -e "\e[92m"
+      /bin/echo -e "\e[92m"
         if [ `grep -c /etc/motd /root/.bashrc` == 0 ]
          then
-            echo -e " \e[1;31m code bash missing"
-            echo "cat /etc/motd" >> /root/.bashrc
-            echo -e "\e[92m - Code bash \e[0madded.\n"
+            /bin/echo -e " \e[1;31m code bash missing"
+            /bin/echo "cat /etc/motd" >> /root/.bashrc
+            /bin/echo -e "\e[92m - Code bash \e[0madded.\n"
          else
            grepmot=`grep  '/etc/motd' /root/.bashrc`
-           echo -e "  \e[1;30m Code bash found: \e[0m>>\e[0m $grepmot \e[0m<<\n"
-           echo -e "  -->\e[34m  Code bash: \e[92mOK\e[0m"
+           /bin/echo -e "  \e[1;30m Code bash found: \e[0m>>\e[0m $grepmot \e[0m<<\n"
+           /bin/echo -e "  -->\e[34m  Code bash: \e[92mOK\e[0m"
         fi
 
  # Check if MOTDs32 is already add
@@ -158,54 +158,54 @@ tput cup 11 0
 
         if [ -f  "/etc/motds32/Stats32" ]
          then
-           echo -e "  -->\e[34m  Stats32 \e[92mFOUND\e[0m"
+           /bin/echo -e "  -->\e[34m  Stats32 \e[92mFOUND\e[0m"
            cp  Stats32 /etc/motds32/Stats32 -Rf
          else
           cp  Stats32 /etc/motds32/Stats32 -Rf
-          echo -e "  -->\e[34m  Stats32 \e[92mINSTALLED\e[0m"
+          /bin/echo -e "  -->\e[34m  Stats32 \e[92mINSTALLED\e[0m"
         fi
 
  # Copy font figlet
         if [ -f  "/usr/share/figlet" ]
          then
-           echo -e "  -->\e[31m  figlet not FOUND\e[0m"
+           /bin/echo -e "  -->\e[31m  figlet not FOUND\e[0m"
          else
            cp -Rf fonts/chunky.flf /usr/share/figlet/
-           echo -e "  -->\e[34m  Font figlet \e[92mOK\e[0m"
+           /bin/echo -e "  -->\e[34m  Font figlet \e[92mOK\e[0m"
         fi
 
  # Check if motd is installed correctly
         if [ -f  "/usr/bin/motd" ]
          then
-           echo -e "  -->\e[34m  motd check \e[92mOK\e[0m\n"
+           /bin/echo -e "  -->\e[34m  motd check \e[92mOK\e[0m\n"
          else
             ln -s /usr/bin/motds32 /usr/bin/motd
-            echo -e "  -->\e[34m  motd bin \e[92mOK\e[0m\n"
+            /bin/echo -e "  -->\e[34m  motd bin \e[92mOK\e[0m\n"
         fi
  ## Install Crontab
         if crontab -l | grep -q '/usr/bin/motds32';  then
-          echo -e " \e[34m -Cron \e[0m>> already added.\e[0m"
+          /bin/echo -e " \e[34m -Cron \e[0m>> already added.\e[0m"
         else
-          echo -e "\e[34m - Cron added.\e[0m"
-        (crontab -l ; echo "*/5 * * * *      /usr/bin/motds32 -g 2>1")| crontab -
+          /bin/echo -e "\e[34m - Cron added.\e[0m"
+        (crontab -l ; /bin/echo "*/5 * * * *      /usr/bin/motds32 -g 2>1")| crontab -
         fi
 ## Generate first stats
 if test -f /usr/bin/motds32; then /usr/bin/motds32 -g; fi
 
-      echo -e "\n\n\e[34m \e[92m  Ｉｎｓｔａｌｌａｔｉｏｎ  ｏｆ  ＭＯＴＤｓ３２ completed!\e[0m  \n\n\n Help? Use: \n \033[1;30m /usr/bin/motds32\e[0m \n or \033[1;30m\n  motd\e[0m\n"
+      /bin/echo -e "\n\n\e[34m \e[92m  Ｉｎｓｔａｌｌａｔｉｏｎ  ｏｆ  ＭＯＴＤｓ３２ completed!\e[0m  \n\n\n Help? Use: \n \033[1;30m /usr/bin/motds32\e[0m \n or \033[1;30m\n  motd\e[0m\n"
       printf "\n   \e[1;33m  (Press 0 to exit)\e[0m"
       printf "\n   \e[1;33m  (Or any key to go back.)\e[0m" 
        ;;
-        2)  echo -e "\n\e[92m"
+        2)  /bin/echo -e "\n\e[92m"
         if [ `grep -c /etc/motd /root/.bashrc` == 0 ]
          then
-            echo -e " code bash missing"
-            echo "cat /etc/motd" >> /root/.bashrc
-            echo -e "\e[92m - Code bash \e[0madded.\n"
+            /bin/echo -e " code bash missing"
+            /bin/echo "cat /etc/motd" >> /root/.bashrc
+            /bin/echo -e "\e[92m - Code bash \e[0madded.\n"
          else
            grepmot=`grep  '/etc/motd' /root/.bashrc`
-           echo -e "  \e[1;30m Code bash found: \e[0m>>\e[0m $grepmot \e[0m<<\n"
-           echo -e "  -->\e[34m  Code bash: \e[92mOK\e[0m"
+           /bin/echo -e "  \e[1;30m Code bash found: \e[0m>>\e[0m $grepmot \e[0m<<\n"
+           /bin/echo -e "  -->\e[34m  Code bash: \e[92mOK\e[0m"
         fi
 
 # Check if MOTDs32 is already add
@@ -213,56 +213,56 @@ if test -f /usr/bin/motds32; then /usr/bin/motds32 -g; fi
         
         if [ -f "/usr/bin/motds32" ]
       then
-       echo -e "\n\e[34m - Make file \e[0m>> founded.\e[0m\n"
+       /bin/echo -e "\n\e[34m - Make file \e[0m>> founded.\e[0m\n"
        #make install
-       #echo -e "\n\e[34m - REMake file done.\e[0m\n"
+       #/bin/echo -e "\n\e[34m - REMake file done.\e[0m\n"
       else
-       echo -e "\n\e[92m -Building binary \e[0m\n"
+       /bin/echo -e "\n\e[92m -Building binary \e[0m\n"
        make install
-       echo -e "\n\e[34m - Make file done.\e[0m\n"
+       /bin/echo -e "\n\e[34m - Make file done.\e[0m\n"
      fi
      
         if [ -f  "/etc/motds32/Stats32" ]
          then
-           echo -e "  -->\e[34m  Stats32 \e[92mFOUND\e[0m"
+           /bin/echo -e "  -->\e[34m  Stats32 \e[92mFOUND\e[0m"
            cp  Stats32 /etc/motds32/Stats32 -Rf
          else
           cp  Stats32 /etc/motds32/Stats32 -Rf
-          echo -e "  -->\e[34m  Stats32 \e[92mINSTALLED\e[0m"
+          /bin/echo -e "  -->\e[34m  Stats32 \e[92mINSTALLED\e[0m"
         fi
 
  # Copy font figlet
         if [ -f  "/usr/share/figlet" ]
          then
-           echo -e "  -->\e[31m  figlet not FOUND\e[0m"
+           /bin/echo -e "  -->\e[31m  figlet not FOUND\e[0m"
          else
            cp -Rf fonts/chunky.flf /usr/share/figlet/
-           echo -e "  -->\e[34m  Font figlet \e[92mOK\e[0m"
+           /bin/echo -e "  -->\e[34m  Font figlet \e[92mOK\e[0m"
         fi
 
  # Check if motd  ins talled correctly
         if [ -f  "/usr/bin/motd" ]
          then
-           echo -e "  -->\e[34m  motd check \e[92mOK\e[0m\n"
+           /bin/echo -e "  -->\e[34m  motd check \e[92mOK\e[0m\n"
          else
             ln -s /usr/bin/motds32 /usr/bin/motd
-            echo -e "  -->\e[34m  motd bin \e[92mOK\e[0m\n"
+            /bin/echo -e "  -->\e[34m  motd bin \e[92mOK\e[0m\n"
         fi
 
  ## Install Crontab
         if crontab -l | grep -q '/usr/bin/motds32';  then
-          echo -e "\e[34m - Cron \e[0m>> already added.\e[0m"
+          /bin/echo -e "\e[34m - Cron \e[0m>> already added.\e[0m"
         else
-          echo -e "\e[34m - Cron added.\e[0m"
-          (crontab -l ; echo "*/5 * * * *      /usr/bin/motds32 -g 2>1")| crontab -
+          /bin/echo -e "\e[34m - Cron added.\e[0m"
+          (crontab -l ; /bin/echo "*/5 * * * *      /usr/bin/motds32 -g 2>1")| crontab -
         fi
 ## Generate first stats
       if test -f /usr/bin/motds32; then /usr/bin/motds32 -g; fi
 
-      echo -e "\n\n\e[34m \e[92m  Ｉｎｓｔａｌｌａｔｉｏｎ  ｏｆ  ＭＯＴＤｓ３２ completed!\e[0m  \n\n\n Help? Use: \n \033[1;30m /usr/bin/motds32\e[0m \n or \033[1;30m\n  motd\e[0m\n"
+      /bin/echo -e "\n\n\e[34m \e[92m  Ｉｎｓｔａｌｌａｔｉｏｎ  ｏｆ  ＭＯＴＤｓ３２ completed!\e[0m  \n\n\n Help? Use: \n \033[1;30m /usr/bin/motds32\e[0m \n or \033[1;30m\n  motd\e[0m\n"
       printf "\n   \e[1;33m  (Press any key to exit.)\e[0m"
             ;;
-        3)  echo -e "\n\e[34m Dsinstallation of ＭＯＴＤｓ３２ in progress... \e[0m"
+        3)  /bin/echo -e "\n\e[34m Dsinstallation of ＭＯＴＤｓ３２ in progress... \e[0m"
 ## remove file and directory
           make uninstall
           rm /etc/motds32/ -Rf
@@ -271,24 +271,24 @@ if test -f /usr/bin/motds32; then /usr/bin/motds32 -g; fi
 
 ## remove cron
          if crontab -l | grep -q '/usr/bin/motds32';  then
-         echo -e "  \e[92m Cron localized\n"
+         /bin/echo -e "  \e[92m Cron localized\n"
     crontab -l | grep -v '/usr/bin/motds32 -g 2>1' | crontab -u root -
-    echo -e "  \e[92m Cron removed\n"
+    /bin/echo -e "  \e[92m Cron removed\n"
     else
-    echo -e "  \e[93m Cron not find\n"
+    /bin/echo -e "  \e[93m Cron not find\n"
     fi
 
     if [ `grep -c /etc/motd /root/.bashrc` == 0 ]
     then
-     echo "  Code bash not founded"
+     /bin/echo "  Code bash not founded"
     else
-     echo "  code bash founded"
+     /bin/echo "  code bash founded"
      sed -i '/cat \/etc\/motd/d' /root/.bashrc
-     echo -e "\e[92m Code .bashrc remove.\e[0m"
+     /bin/echo -e "\e[92m Code .bashrc remove.\e[0m"
      grep -n  '/etc/motd' /root/.bashrc
     fi
 
-	echo -e "\n\e[92m Script ＭＯＴＤｓ３２ has been completly removed \e[0m"
+	/bin/echo -e "\n\e[92m Script ＭＯＴＤｓ３２ has been completly removed \e[0m"
 	printf "\n   \e[1;33m  (Press any key.)\e[0m"
             ;;
         0)  break
@@ -298,11 +298,11 @@ if test -f /usr/bin/motds32; then /usr/bin/motds32 -g; fi
       esac
       read -n 1
 
-#      echo -e "\n\n\e[34m \e[92m  Ｉｎｓｔａｌｌａｔｉｏｎ  ｏｆ  ＭＯＴＤｓ３２ completed!\e[0m  \n\n\n Help? Use: \n \033[1;30m /usr/bin/motds32\e[0m \n or \033[1;30m\n  motd\e[0m\n"
+#      /bin/echo -e "\n\n\e[34m \e[92m  Ｉｎｓｔａｌｌａｔｉｏｎ  ｏｆ  ＭＯＴＤｓ３２ completed!\e[0m  \n\n\n Help? Use: \n \033[1;30m /usr/bin/motds32\e[0m \n or \033[1;30m\n  motd\e[0m\n"
 #      printf "\n   \e[1;33m  (Press any key to exit.)\e[0m"
 
 done
 
 # Restore screen
 tput rmcup
-echo "Installation MOTDs32 terminated."
+/bin/echo "Installation MOTDs32 terminated."
